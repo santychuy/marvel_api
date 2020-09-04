d200d0accff8f3ddbf755be697f65afab7212195
Santiago Carrasco <jsanticc@hotmail.com>
2020-09-04 15:06:16 -0700

    Important message

--- src/helpers/validCharacterId.ts
+++ src/helpers/validCharacterId.ts
@@ -1,5 +1,8 @@
 import { ValidationCharacterId } from '../ts/interfaces';
 
+// Si se quiere agregar un nuevo personaje, se valida el nombre del personaje,
+// y se regresa un objeto con el id y un booleano en verdadero
+
 export default (character: string): ValidationCharacterId => {
   if (character === 'ironman') {
     return { characterId: '1009368', valid: true };
